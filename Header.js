358d4cffcddc098f8f3a38462919941b8db2ff83
szaszfaijulia <szaszfai.julia@gmail.com>
2022-04-07 22:42:43 +0200

    2 files modified, 2 added, 1 deleted

--- Header.js
+++ Header.js
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Header() {
   return(
     <header>
-      <nav className="nav" style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-        <img src="https://raw.githubusercontent.com/szaszfaijulia/react-staticPage1/master/react_logo.png" style={{width: '70px'}} />
-        <ul className="nav-items" style={{display: 'flex', listStyle: 'none'}}>
-          <li style={{padding: '10px'}}>Pricing</li>
-          <li style={{padding: '10px'}}>About</li>
-          <li style={{padding: '10px'}}>Contact</li>
+      <nav className="nav">
+        <img src="https://raw.githubusercontent.com/szaszfaijulia/react-staticPage1/master/react_logo.png" className="nav-logo"/>
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
         </ul>
       </nav>
     </header>
--- index/index08.js
+++ index/index08.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./style.css"
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
